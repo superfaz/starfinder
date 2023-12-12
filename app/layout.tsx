0adacae29ad83a4da68d638010dfdc948b298491
superfaz <16510828+superfaz@users.noreply.github.com>
2023-12-12 14:39:57 +0100

    Design review

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,10 @@ export default async function RootLayout({ children }: { children: React.ReactNo
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+
+        {/* eslint-disable-next-line @next/next/google-font-display */}
         <link
-          href="https://fonts.googleapis.com/css2?family=Comfortaa&family=Orbitron&display=optional"
+          href="https://fonts.googleapis.com/css2?family=Comfortaa&family=Orbitron&display=block"
           rel="stylesheet"
         />
       </head>
