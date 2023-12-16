d35180bddcc03d7d98eca7a29efc15106e9429d7
superfaz <16510828+superfaz@users.noreply.github.com>
2023-12-16 09:37:07 +0100

    Vitest switch to happy-dom

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: "jsdom",
+    environment: "happy-dom",
     setupFiles: ["./vitest.setup.ts"],
     coverage: {
       provider: "istanbul", // or 'v8',
