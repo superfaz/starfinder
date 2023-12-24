4499c7dc39e3f426a8fb7727a8712ea453d75d20
superfaz <16510828+superfaz@users.noreply.github.com>
2023-12-24 14:23:44 +0100

    Move data initialisation

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,60 +1,8 @@
-import { AbilityScores, Alignments, Avatars, Classes, DataSet, Races, Skills, Specials, Themes } from "data";
-import { AbilityScore, Alignment, Avatar, Class, Race, SkillDefinition, Special, Theme } from "model";
+import { DataSet, buildDataSet } from "data";
 import { ClientComponent } from "./client";
 
-function getRaces(): Race[] {
-  return Races;
-}
-
-function getThemes(): Theme[] {
-  return Themes;
-}
-
-function getClasses(): Class[] {
-  return Classes;
-}
-
-function getSkills(): SkillDefinition[] {
-  return Skills;
-}
-
-function getAbilityScores(): AbilityScore[] {
-  return AbilityScores;
-}
-
-function getSpecials(): Record<string, Special> {
-  return Specials;
-}
-
-function getAlignments(): Alignment[] {
-  return Alignments;
-}
-
-function getAvatars(): Avatar[] {
-  return Avatars;
-}
-
 export default function Page() {
-  const data: DataSet = {
-    abilityScores: getAbilityScores(),
-    alignments: getAlignments(),
-    avatars: getAvatars(),
-    classes: getClasses(),
-    races: getRaces(),
-    themes: getThemes(),
-    skills: getSkills(),
-    specials: getSpecials(),
-    armors: { light: "Légère", heavy: "Lourde" },
-    weapons: {
-      basic: "Armes de corps à corps simples",
-      advanced: "Armes de corps à corps évoluées",
-      small: "Armes légères",
-      long: "Armes longues",
-      heavy: "Armes lourdes",
-      sniper: "Armes de précision",
-      grenade: "Grenades",
-    },
-  };
+  const data: DataSet = buildDataSet();
 
   return (
     <>
--- data/DataSet.tsx
+++ data/DataSet.tsx
@@ -1,4 +1,5 @@
 import { AbilityScore, Alignment, Avatar, Class, Race, SkillDefinition, Special, Theme } from "model";
+import { AbilityScores, Alignments, Avatars, Classes, Races, Skills, Specials, Themes } from ".";
 
 export interface DataSet {
   abilityScores: AbilityScore[];
@@ -11,4 +12,61 @@ export interface DataSet {
   themes: Theme[];
   armors: Record<string, string>;
   weapons: Record<string, string>;
+}
+
+function getRaces(): Race[] {
+  return Races;
+}
+
+function getThemes(): Theme[] {
+  return Themes;
+}
+
+function getClasses(): Class[] {
+  return Classes;
+}
+
+function getSkills(): SkillDefinition[] {
+  return Skills;
+}
+
+function getAbilityScores(): AbilityScore[] {
+  return AbilityScores;
+}
+
+function getSpecials(): Record<string, Special> {
+  return Specials;
+}
+
+function getAlignments(): Alignment[] {
+  return Alignments;
+}
+
+function getAvatars(): Avatar[] {
+  return Avatars;
+}
+
+export function buildDataSet(): DataSet {
+  const data: DataSet = {
+    abilityScores: getAbilityScores(),
+    alignments: getAlignments(),
+    avatars: getAvatars(),
+    classes: getClasses(),
+    races: getRaces(),
+    themes: getThemes(),
+    skills: getSkills(),
+    specials: getSpecials(),
+    armors: { light: "Légère", heavy: "Lourde" },
+    weapons: {
+      basic: "Armes de corps à corps simples",
+      advanced: "Armes de corps à corps évoluées",
+      small: "Armes légères",
+      long: "Armes longues",
+      heavy: "Armes lourdes",
+      sniper: "Armes de précision",
+      grenade: "Grenades",
+    },
+  };
+
+  return data;
 }
