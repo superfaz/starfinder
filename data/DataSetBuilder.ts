b0d3b6ecffa1e0cef4bf3402bd27daf4ab890c18
superfaz <16510828+superfaz@users.noreply.github.com>
2024-01-01 12:06:56 +0100

    Add configuration for database connection

--- data/DataSetBuilder.ts
+++ data/DataSetBuilder.ts
@@ -26,8 +26,8 @@ export class DataSetBuilder {
 
   constructor() {
     this.client = new CosmosClient({
-      endpoint: "https://localhost:8081",
-      key: "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==",
+      endpoint: process.env.STARFINDER_COSMOS_ENDPOINT ?? "https://localhost:8081",
+      key: process.env.STARFINDER_COSMOS_KEY_RO ?? "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==",
     });
     this.database = this.client.database("starfinder");
   }
