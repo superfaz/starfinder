f23b5450dbd0282be9675e5cc40d4dcdce1b02f0
superfaz <16510828+superfaz@users.noreply.github.com>
2024-01-12 22:20:01 +0100

    Finalize data clean-up

--- jest.setup.ts
+++ jest.setup.ts
@@ -3,7 +3,8 @@ import * as fs from "fs/promises";
 import { beforeAll, jest } from "@jest/globals";
 import { DataSetBuilder, IDataSet } from "data";
 import { addFetchMock, mockFetch } from "./mocks/fetch";
-import operativeDetails from "./data/class-operative.json";
+import operativeDetails from "./mocks/class-operative.json";
+import envoyDetails from "./mocks/class-envoy.json";
 import scholarDetails from "./mocks/themes-details.json";
 
 async function readFile(path: string) {
@@ -16,7 +17,7 @@ async function mockBuild(): Promise<IDataSet> {
     getAlignments: async () => readFile("./mocks/alignments.json"),
     getAvatars: async () => readFile("./mocks/avatars.json"),
     getClasses: async () => readFile("./mocks/classes.json"),
-    getClassDetails: async () => readFile("./data/class-operative.json"),
+    getClassDetails: async (classId) => readFile(`./mocks/class-${classId}.json`),
     getRaces: async () => readFile("./mocks/races.json"),
     getSkills: async () => readFile("./mocks/skills.json"),
     getThemes: async () => readFile("./mocks/themes.json"),
@@ -32,5 +33,6 @@ beforeAll(() => {
   jest.spyOn(DataSetBuilder.prototype, "build").mockImplementation(mockBuild);
 
   addFetchMock("/api/classes/operative/details", operativeDetails);
+  addFetchMock("/api/classes/envoy/details", envoyDetails);
   addFetchMock("/api/themes/scholar", scholarDetails);
 });
