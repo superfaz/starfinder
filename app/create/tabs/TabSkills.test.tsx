a524cee00e37e3495ee82bed40ba0cde4080d834
superfaz <16510828+superfaz@users.noreply.github.com>
2024-01-12 23:09:35 +0100

    Add a test on class skills

--- app/create/tabs/TabSkills.test.tsx
+++ app/create/tabs/TabSkills.test.tsx
@@ -46,6 +46,78 @@ describe("TabSkills", () => {
     expect(content.getByRole("spinbutton", { name: "Charisme -2" })).toHaveProperty("value", "8");
   });
 
+  const classes = [
+    {
+      id: "operative",
+      classSkills: [
+        "Acrobaties (DEX)",
+        "Athlétisme (FOR)",
+        "Bluff (CHA)",
+        "Culture (INT)",
+        "Déguisement (CHA)",
+        "Discrétion (DEX)",
+        "Escamotage (DEX)",
+        "Informatique (INT)",
+        "Ingénierie (INT)",
+        "Intimidation (CHA)",
+        "Médecine (INT)",
+        "Perception (SAG)",
+        "Pilotage (DEX)",
+        "Profession",
+        "Psychologie (SAG)",
+        "Survie (SAG)",
+      ],
+    },
+    {
+      id: "soldier",
+      classSkills: [
+        "Acrobaties (DEX)",
+        "Athlétisme (FOR)",
+        "Ingénierie (INT)",
+        "Intimidation (CHA)",
+        "Médecine (INT)",
+        "Pilotage (DEX)",
+        "Profession",
+        "Survie (SAG)",
+      ],
+    },
+  ];
+
+  const themes = [
+    {
+      id: "bounty-hunter",
+      classSkills: ["Survie (SAG)"],
+    },
+    {
+      id: "scholar",
+      classSkills: ["Science de la vie (INT)"],
+    },
+    {
+      id: "themeless",
+      classSkills: [],
+    },
+  ];
+
+  const matrix = classes.flatMap((klass) => themes.map((theme) => ({ klass, theme })));
+  test.each(matrix)(
+    "displays the class skills based on theme '$theme.id' and class '$klass.id'",
+    async ({ theme, klass }) => {
+      const user = userEvent.setup();
+      await user.click(screen.getByRole("button", { name: "Classe" }));
+      await user.selectOptions(screen.getByRole("combobox", { name: "Classe" }), klass.id);
+      await user.click(screen.getByRole("button", { name: "Thème" }));
+      await user.selectOptions(screen.getByRole("combobox", { name: "Thème" }), theme.id);
+      await user.click(screen.getByRole("button", { name: "Caractéristiques & Compétences" }));
+
+      const content = within(document.querySelector("#content") as HTMLElement);
+
+      for (const skill of [...klass.classSkills, ...theme.classSkills]) {
+        expect(content.queryByRole("checkbox", { name: skill })).not.toBeNull();
+        expect(content.getByRole("checkbox", { name: skill })).toBeChecked();
+      }
+    }
+  );
+
   test.failing("add +1 when the theme skill is already a class skill", async () => {
     const content = within(document.querySelector("#content") as HTMLElement);
     const view = within(content.getByTestId("surv"));
