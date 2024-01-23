243ac12cc89e4bcca0929fdc6f77bb767044b063
superfaz <16510828+superfaz@users.noreply.github.com>
2024-01-23 22:27:43 +0100

    Fix IModel to support 1 letter ids

--- model/IModel.ts
+++ model/IModel.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const IModel = z.object({
-  id: z.union([z.string().regex(/^[a-z][a-z0-9-]*[a-z0-9]$/), z.string().uuid()]),
+  id: z.union([z.string().regex(/^([a-z]|[a-z][a-z0-9-]*[a-z0-9])$/), z.string().uuid()]),
 });
 
 export type IModel = z.infer<typeof IModel>;
