1919d0303be94ca148390032b6b936f98c10344e
superfaz <16510828+superfaz@users.noreply.github.com>
2024-01-31 14:24:09 +0100

    Review jest mock for DataSetBuilder.build

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,5 +1,4 @@
 import "@testing-library/jest-dom";
-import * as fs from "fs/promises";
 import { beforeAll, jest } from "@jest/globals";
 import { DataSetBuilder, IDataSet } from "data";
 import { addFetchMock, mockFetch } from "./mocks/fetch";
@@ -7,26 +6,36 @@ import envoyDetails from "./mocks/class-envoy.json";
 import operativeDetails from "./mocks/class-operative.json";
 import scholarDetails from "./mocks/themes-details.json";
 import soldierDetails from "./mocks/class-soldier.json";
-
-async function readFile(path: string) {
-  return fs.readFile(path, "utf-8").then((data: string) => JSON.parse(data));
-}
+import {
+  AbilityScore,
+  Alignment,
+  Armor,
+  Avatar,
+  Class,
+  FeatTemplate,
+  Race,
+  SavingThrow,
+  SkillDefinition,
+  Theme,
+  ThemeScholar,
+  Weapon,
+} from "model";
 
 async function mockBuild(): Promise<IDataSet> {
   return {
-    getAbilityScores: async () => readFile("./mocks/ability-scores.json"),
-    getAlignments: async () => readFile("./mocks/alignments.json"),
-    getAvatars: async () => readFile("./mocks/avatars.json"),
-    getClasses: async () => readFile("./mocks/classes.json"),
-    getClassDetails: async (classId) => readFile(`./mocks/class-${classId}.json`),
-    getFeats: async () => readFile("./mocks/feats.json"),
-    getRaces: async () => readFile("./mocks/races.json"),
-    getSavingThrows: async () => readFile("./mocks/saving-throws.json"),
-    getSkills: async () => readFile("./mocks/skills.json"),
-    getThemes: async () => readFile("./mocks/themes.json"),
-    getThemeDetails: async () => readFile("./mocks/themes-details.json"),
-    getArmors: async () => readFile("./mocks/armors.json"),
-    getWeapons: async () => readFile("./mocks/weapons.json"),
+    getAbilityScores: async () => AbilityScore.array().parse((await import("./mocks/ability-scores.json")).default),
+    getAlignments: async () => Alignment.array().parse((await import("./mocks/alignments.json")).default),
+    getAvatars: async () => Avatar.array().parse((await import("./mocks/avatars.json")).default),
+    getClasses: async () => Class.array().parse((await import("./mocks/classes.json")).default),
+    getClassDetails: async (classId) => (await import(`./mocks/class-${classId}.json`)).default,
+    getRaces: async () => Race.array().parse((await import("./mocks/races.json")).default),
+    getFeats: async () => FeatTemplate.array().parse((await import("./mocks/feats.json")).default),
+    getThemes: async () => Theme.array().parse((await import("./mocks/themes.json")).default),
+    getThemeDetails: async () => ThemeScholar.parse((await import("./mocks/themes-details.json")).default),
+    getSavingThrows: async () => SavingThrow.array().parse((await import("./mocks/saving-throws.json")).default),
+    getSkills: async () => SkillDefinition.array().parse((await import("./mocks/skills.json")).default),
+    getArmors: async () => Armor.array().parse((await import("./mocks/armors.json")).default),
+    getWeapons: async () => Weapon.array().parse((await import("./mocks/weapons.json")).default),
   };
 }
 
