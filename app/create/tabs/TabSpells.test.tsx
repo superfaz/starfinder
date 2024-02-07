dbe0ceb738632441b4ea6aca1846129f452e064b
superfaz <16510828+superfaz@users.noreply.github.com>
2024-02-07 12:20:51 +0100

    Spells - Improve display

--- app/create/tabs/TabSpells.test.tsx
+++ app/create/tabs/TabSpells.test.tsx
@@ -12,8 +12,7 @@ describe("TabSpells", () => {
   });
 
   test("is not displayed", async () => {
-    const content = within(document.querySelector("#content") as HTMLElement);
-    expect(content.queryByRole("heading", { level: 2, name: /Sorts de niveau 0/ })).toBeNull();
+    expect(screen.queryByRole("heading", { level: 2, name: /Sorts de niveau 0/ })).toBeNull();
   });
 
   test("is not displayed for soldier", async () => {
@@ -26,8 +25,7 @@ describe("TabSpells", () => {
     await user.selectOptions(screen.getByRole("combobox", { name: "Classe" }), "soldier");
     await user.click(screen.getByRole("button", { name: "Sorts" }));
 
-    const content = within(document.querySelector("#content") as HTMLElement);
-    expect(content.queryByRole("heading", { level: 2, name: /Sorts de niveau 0/ })).toBeNull();
+    expect(screen.queryByRole("heading", { level: 2, name: /Sorts de niveau 0/ })).toBeNull();
   });
 });
 
@@ -47,7 +45,25 @@ describe("TabSpells", () => {
   });
 
   test("is displayed", async () => {
-    const content = within(document.querySelector("#content") as HTMLElement);
-    expect(content.queryByRole("heading", { level: 2, name: /Sorts de niveau 0/ })).not.toBeNull();
+    expect(screen.queryByRole("heading", { level: 2, name: /Sorts de niveau 0/ })).not.toBeNull();
+  });
+
+  test.each([0, 1])("displays filtered spells '%s'", async (level) => {
+    const content = within(screen.getByTestId("spells-" + level));
+    expect(content.queryByText("Shared level " + level), "shared spell not displayed").not.toBeNull();
+    expect(content.queryByText("Mystic level " + level), "mystic spell not displayed").not.toBeNull();
+    expect(content.queryByText("Technomancer level " + level), "technomancer spell displayed").toBeNull();
+  });
+
+  test.each([2, 3, 4, 5, 6])("displays filtered ranged spells '%s'", async (level) => {
+    const content = within(screen.getByTestId("spells-" + level));
+    expect(content.queryByText("Shared ranged " + level), "shared ranged not displayed").not.toBeNull();
+    expect(content.queryByText("Mystic ranged " + level), "mystic ranged not displayed").not.toBeNull();
+    expect(content.queryByText("Technomancer ranged " + level), "technomancer ranged displayed").toBeNull();
+  });
+
+  test.each([2, 3, 4, 5, 6])("displays evolutive spells", async (level) => {
+    const content = within(screen.getByTestId("spells-" + level));
+    expect(content.getByText(`Spell level 2 to 6 with ${level * 2}d6.`), "template not applied").toBeDefined();
   });
 });
--- app/create/tabs/TabSpells.tsx
+++ app/create/tabs/TabSpells.tsx
@@ -1,4 +1,4 @@
-import { useAppSelector } from "logic";
+import { CharacterPresenter, useAppSelector } from "logic";
 import { Card, Col, Row, Stack } from "react-bootstrap";
 import { CharacterProps } from "../Props";
 import { useMemo } from "react";
@@ -20,6 +20,54 @@ function filterOnLevelAndClass(classId: CasterId, level: number) {
   };
 }
 
+type SpellComponentProps = {
+  character: CharacterPresenter;
+  spell: Spell;
+  classId: CasterId;
+  level: number;
+};
+function SpellComponent({ character, spell, classId, level }: SpellComponentProps) {
+  const def = spell.levels[classId];
+  if (def === undefined) {
+    console.log("Spell can't be displayed - no level information");
+    return null;
+  }
+  if (typeof def === "number") {
+    return (
+      <Card key={spell.id}>
+        <Card.Header>{spell.name}</Card.Header>
+        <Card.Body>
+          <span className="text-muted">{spell.description}</span>
+        </Card.Body>
+      </Card>
+    );
+  } else if (spell.evolutions !== undefined) {
+    const templater = character.createTemplater({ ...spell.evolutions[level.toString()] });
+    const description = templater.convertString(spell.description);
+    return (
+      <Card key={spell.id}>
+        <Card.Header>
+          {spell.name} {level}
+        </Card.Header>
+        <Card.Body>
+          <span className="text-muted">{description}</span>
+        </Card.Body>
+      </Card>
+    );
+  } else {
+    return (
+      <Card key={spell.id}>
+        <Card.Header>
+          {spell.name} {level}
+        </Card.Header>
+        <Card.Body>
+          <span className="text-muted">{spell.description}</span>
+        </Card.Body>
+      </Card>
+    );
+  }
+}
+
 export function SpellsSelection({ character }: CharacterProps) {
   const data = useAppSelector((state) => state.data);
   const classId = character.getClass()?.id;
@@ -36,7 +84,7 @@ export function SpellsSelection({ character }: CharacterProps) {
   const levels = [0, 1, 2, 3, 4, 5, 6];
 
   return levels.map((level) => (
-    <Row key={level}>
+    <Row key={level} data-testid={"spells-" + level}>
       <Col xs="3">
         <Stack direction="vertical" gap={2}>
           <h2>Sélection</h2>
@@ -48,12 +96,7 @@ export function SpellsSelection({ character }: CharacterProps) {
           <Row>
             {availableSpells.filter(filterOnLevelAndClass(classId, level)).map((spell) => (
               <Col key={spell.id} lg={4} className="mb-3">
-                <Card key={spell.id}>
-                  <Card.Header>{spell.name}</Card.Header>
-                  <Card.Body>
-                    <span className="text-muted">{spell.description}</span>
-                  </Card.Body>
-                </Card>
+                <SpellComponent character={character} spell={spell} classId={classId} level={level} />
               </Col>
             ))}
           </Row>
