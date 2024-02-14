9b6bf5ea84d9d635f275723028704e687d93a3c5
superfaz <16510828+superfaz@users.noreply.github.com>
2024-02-14 21:26:28 +0100

    Add damageType in model and datasets

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -6,6 +6,7 @@ import {
   Armor,
   Avatar,
   Class,
+  DamageType,
   FeatTemplate,
   Profession,
   Race,
@@ -33,19 +34,20 @@ async function mockBuild(): Promise<IDataSet> {
   return {
     getAbilityScores: async () => AbilityScore.array().parse((await import("./mocks/ability-scores.json")).default),
     getAlignments: async () => Alignment.array().parse((await import("./mocks/alignments.json")).default),
+    getArmors: async () => Armor.array().parse((await import("./mocks/armors.json")).default),
     getAvatars: async () => Avatar.array().parse((await import("./mocks/avatars.json")).default),
     getClasses: async () => Class.array().parse((await import("./mocks/classes.json")).default),
     getClassDetails: async (classId) => (await import(`./mocks/class-${classId}.json`)).default,
-    getRaces: async () => Race.array().parse((await import("./mocks/races.json")).default),
+    getDamageTypes: async () => DamageType.array().parse((await import("./mocks/damage-types.json")).default),
     getFeats: async () => FeatTemplate.array().parse((await import("./mocks/feats.json")).default),
+    getProfessions: async () => Profession.array().parse((await import("./mocks/professions.json")).default),
+    getRaces: async () => Race.array().parse((await import("./mocks/races.json")).default),
     getSpells: async () => Spell.array().parse((await import("./mocks/spells.json")).default),
     getThemes: async () => Theme.array().parse((await import("./mocks/themes.json")).default),
     getThemeDetails: async () => ThemeScholar.parse((await import("./mocks/themes-details.json")).default),
     getSavingThrows: async () => SavingThrow.array().parse((await import("./mocks/saving-throws.json")).default),
     getSkills: async () => SkillDefinition.array().parse((await import("./mocks/skills.json")).default),
-    getArmors: async () => Armor.array().parse((await import("./mocks/armors.json")).default),
     getWeapons: async () => Weapon.array().parse((await import("./mocks/weapons.json")).default),
-    getProfessions: async () => Profession.array().parse((await import("./mocks/professions.json")).default),
   };
 }
 
