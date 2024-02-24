341c211131614857ce635b69df18174054ca7c26
superfaz <16510828+superfaz@users.noreply.github.com>
2024-02-24 07:49:45 +0100

    Fix e2e tests

--- e2e/home.spec.ts
+++ e2e/home.spec.ts
@@ -2,14 +2,14 @@ import { test, expect } from "@playwright/test";
 
 test("has title", async ({ page }) => {
   await page.goto("/");
-  await expect(page.getByRole("heading", { name: /Bienvenue !/, level: 1 })).toBeVisible();
+  await expect(page.getByRole("heading", { name: /Bienvenue à toi/, level: 1 })).toBeVisible();
 });
 
 test("has create link", async ({ page }) => {
   await page.goto("/");
 
   // Click the create link
-  await page.getByRole("link", { name: /Créer un personnage/ }).click();
+  await page.getByRole("link", { name: /Démarrer la création/ }).click();
 
   // Expect a navigation to the create page
   await expect(page).toHaveURL(/\/create/);
