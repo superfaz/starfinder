fb2d6415142ded8e27bfdecf577baa947d0f0f73
superfaz <16510828+superfaz@users.noreply.github.com>
2024-03-04 11:24:07 +0100

    Sheet - add spells

--- src/app/create/tabs/TabSheet.tsx
+++ src/app/create/tabs/TabSheet.tsx
@@ -405,6 +405,43 @@ function CardFeats({ character }: CharacterProps) {
   );
 }
 
+function CardSpells({ character }: CharacterProps) {
+  const spellsByLevel = character.getSelectedSpells();
+  const isSpellCaster: boolean = character.getClass()?.spellCaster ?? false;
+  const levels = [0, 1, 2, 3, 4, 5, 6];
+  if (!isSpellCaster) {
+    return null;
+  }
+
+  const spellsCount = Object.values(spellsByLevel).reduce((acc, level) => acc + level.length, 0);
+
+  return (
+    <Card data-testid="sheet-spells">
+      <Card.Header>
+        <Badge bg="primary">Sorts</Badge>
+      </Card.Header>
+      <Card.Body className="small">
+        <Stack gap={2}>
+          {spellsCount === 0 && <em>Pas de sort sélectionné</em>}
+          {levels.map((level) => {
+            const spells = spellsByLevel[level];
+            if (spells !== undefined && spells.length > 0) {
+              return (
+                <>
+                  <Badge className="bg-secondary me-auto">Niv. {level}</Badge>
+                  {spells.map((spell) => (
+                    <div key={spell.id}>{spell.name}</div>
+                  ))}
+                </>
+              );
+            }
+          })}
+        </Stack>
+      </Card.Body>
+    </Card>
+  );
+}
+
 export function Sheet({ character }: CharacterProps) {
   const data = useAppSelector((state) => state.data);
 
@@ -437,6 +474,7 @@ export function Sheet({ character }: CharacterProps) {
         <Stack direction="vertical" gap={2}>
           <CardAbilities character={character} />
           <CardFeats character={character} />
+          <CardSpells character={character} />
         </Stack>
       </Col>
     </Row>
