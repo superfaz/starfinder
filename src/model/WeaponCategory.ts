e6c0886ed73eda886fc3ef093162a9e1cf847cfb
superfaz <16510828+superfaz@users.noreply.github.com>
2024-03-05 15:01:56 +0100

    Add weapon category ids

--- src/model/WeaponCategory.ts
+++ src/model/WeaponCategory.ts
@@ -1,7 +1,13 @@
 import { z } from "zod";
 import { INamedModelSchema } from "./INamedModel";
 
-export const WeaponCategorySchema = INamedModelSchema.extend({});
+export const WeaponCategoryIdSchema = z.enum(["cryo", "flame", "laser", "plasma", "projectile", "shock", "sonic"]);
+
+export type WeaponCategoryId = z.infer<typeof WeaponCategoryIdSchema>;
+
+export const WeaponCategorySchema = INamedModelSchema.extend({
+  id: WeaponCategoryIdSchema,
+});
 
 export type WeaponCategory = z.infer<typeof WeaponCategorySchema>;
 
