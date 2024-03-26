8ea580ff9782ea9cd58b1feae81095a015030261
superfaz <16510828+superfaz@users.noreply.github.com>
2024-03-26 08:04:58 +0100

    Update page.test.tsx

--- src/app/create/equipment/page.test.tsx
+++ src/app/create/equipment/page.test.tsx
@@ -37,6 +37,6 @@ describe("/create/equipment", () => {
   test("has 1000 credits", async () => {
     const content = within(document.querySelector("#content") as HTMLElement);
     expect(content.getByLabelText("Capital initial")).toHaveValue("1000");
-    expect(content.getByLabelText("Restant")).toHaveValue("1000");
+    expect(content.getByLabelText("Restant")).toHaveValue(1000);
   });
 });
