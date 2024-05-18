5104e46dc27fe178fe38407d96e05efb73f028fa
superfaz <16510828+superfaz@users.noreply.github.com>
2024-05-18 09:35:21 +0200

    Fix build issue

--- src/app/api/equipment/weapons/fusions/route.ts
+++ src/app/api/equipment/weapons/fusions/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import { DataSets, DataSource, IDataSource } from "data";
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   const dataSource: IDataSource = new DataSource();
   return NextResponse.json(await dataSource.get(DataSets.EquipmentWeaponFusions).getAll());
