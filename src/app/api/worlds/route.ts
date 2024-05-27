fb32c558f332402ec79b3d8d4bd14c975edde271
superfaz <16510828+superfaz@users.noreply.github.com>
2024-05-27 20:58:48 +0200

    Fix api

--- src/app/api/worlds/route.ts
+++ src/app/api/worlds/route.ts
@@ -1,5 +1,7 @@
 import { DataSets, DataSource } from "data";
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   const dataSource = new DataSource();
   const dataset = dataSource.get(DataSets.Worlds);
