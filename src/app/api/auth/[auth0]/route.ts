faa1561c100a38b7604169f6e51a1639a820fc08
superfaz <16510828+superfaz@users.noreply.github.com>
2024-06-29 21:35:29 +0200

    Auth0 - Auth required to create

--- src/app/api/auth/[auth0]/route.ts
+++ src/app/api/auth/[auth0]/route.ts
@@ -1,3 +1,8 @@
 import { handleAuth } from "@auth0/nextjs-auth0";
 
+if (!process.env.AUTH0_BASE_URL) {
+  process.env.AUTH0_BASE_URL = process.env.VERCEL_URL ?? process.env.BASE_URL ?? "http://localhost:3000";
+  console.error("AUTH0_BASE_URL is not set, defaulting to", process.env.AUTH0_BASE_URL);
+}
+
 export const GET = handleAuth();
