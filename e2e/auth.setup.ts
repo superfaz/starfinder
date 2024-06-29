f5bff854c3818439cc58843d03f8108188620c80
superfaz <16510828+superfaz@users.noreply.github.com>
2024-06-29 09:30:24 +0200

    Auth0 - Auth required to create

--- e2e/auth.setup.ts
+++ e2e/auth.setup.ts
@@ -2,17 +2,22 @@ import { test as setup } from "@playwright/test";
 
 const authFile = "playwright/.auth/user.json";
 
-const USER_EMAIL = process.env.PLAYWRIGHT_USER_EMAIL;
-const USER_PASSWORD = process.env.PLAYWRIGHT_USER_PASSWORD;
+const PLAYWRIGHT_USER_EMAIL = process.env.PLAYWRIGHT_USER_EMAIL;
+const PLAYWRIGHT_USER_PASSWORD = process.env.PLAYWRIGHT_USER_PASSWORD;
 
-if (!USER_EMAIL || !USER_PASSWORD) {
-  throw new Error("Please provide PLAYWRIGHT_USER_EMAIL and PLAYWRIGHT_USER_PASSWORD in the environment variables.");
+if (!PLAYWRIGHT_USER_EMAIL) {
+  console.error(process.env);
+  throw new Error("Please provide PLAYWRIGHT_USER_EMAIL in the environment variables.");
+}
+if (!PLAYWRIGHT_USER_PASSWORD) {
+  console.error(process.env);
+  throw new Error("Please provide PLAYWRIGHT_USER_PASSWORD in the environment variables.");
 }
 
 setup("authenticate", async ({ page }) => {
   await page.goto("/api/auth/login");
-  await page.getByLabel("Email address").fill(USER_EMAIL);
-  await page.getByLabel("Password").fill(USER_PASSWORD);
+  await page.getByLabel("Email address").fill(PLAYWRIGHT_USER_EMAIL);
+  await page.getByLabel("Password").fill(PLAYWRIGHT_USER_PASSWORD);
   await page.getByRole("button", { name: "Continue", exact: true }).click();
 
   // Wait until the page receives the cookies.
