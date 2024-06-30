66918187bc4338e09467061db0a972686175fdcc
superfaz <16510828+superfaz@users.noreply.github.com>
2024-06-30 06:51:29 +0200

    Auth0 - Auth required to create

--- src/app/auth0.ts
+++ src/app/auth0.ts
@@ -1,3 +1,8 @@
 import { initAuth0 } from "@auth0/nextjs-auth0";
 
-export default initAuth0({ baseURL: process.env.AUTH0_BASE_URL ?? process.env.VERCEL_URL ?? "http://localhost:3000" });
+let baseURL = process.env.AUTH0_BASE_URL;
+if (!baseURL && process.env.VERCEL_URL) {
+  baseURL = `https://${process.env.VERCEL_URL}`;
+}
+
+export default initAuth0({ baseURL });
