6356f52a77393e44399f8a034f6d9bd0bbe2fb1d
superfaz <16510828+superfaz@users.noreply.github.com>
2024-07-03 21:02:30 +0200

    Vercel testing

--- src/app/debug/page.tsx
+++ src/app/debug/page.tsx
@@ -4,7 +4,12 @@ export default function Page() {
   return (
     <div>
       <h1>Debugging</h1>
-      <pre>{JSON.stringify({ VERCEL_URL: process.env.VERCEL_URL })}</pre>
+      <pre>
+        {JSON.stringify({
+          VERCEL_URL: process.env.VERCEL_URL,
+          VERCEL_PROJECT_PRODUCTION_URL: process.env.VERCEL_PROJECT_PRODUCTION_URL,
+        })}
+      </pre>
     </div>
   );
 }
