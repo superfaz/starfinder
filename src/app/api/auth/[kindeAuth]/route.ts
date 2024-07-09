60589d0098982e36b7c6d951d587b99030c211c5
superfaz <16510828+superfaz@users.noreply.github.com>
2024-07-09 21:59:38 +0200

    Use kinde instead of auth0

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,14 +1,23 @@
+import { has } from "@vercel/edge-config";
 import handleAuth from "./auth";
 import { ClientOptions } from "./types";
 
 async function overrideConfig(): Promise<ClientOptions> {
   if (process.env.VERCEL && process.env.VERCEL_ENV === "production") {
     // Deployment to vercel production
-    return {
-      siteUrl: `https://${process.env.VERCEL_URL}`,
-      postLoginRedirectUrl: `https://${process.env.VERCEL_URL}`,
-      postLogoutRedirectUrl: `https://${process.env.VERCEL_URL}`,
-    };
+    if (process.env.VERCEL_DEPLOYMENT_ID && (await has(process.env.VERCEL_DEPLOYMENT_ID))) {
+      return {
+        siteUrl: `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`,
+        postLoginRedirectUrl: `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`,
+        postLogoutRedirectUrl: `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`,
+      };
+    } else {
+      return {
+        siteUrl: `https://${process.env.VERCEL_URL}`,
+        postLoginRedirectUrl: `https://${process.env.VERCEL_URL}`,
+        postLogoutRedirectUrl: `https://${process.env.VERCEL_URL}`,
+      };
+    }
   } else {
     return {};
   }
