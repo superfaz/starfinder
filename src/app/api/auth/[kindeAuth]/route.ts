4059f2cc529e222d76f2f690d474a790758b5822
superfaz <16510828+superfaz@users.noreply.github.com>
2024-07-10 06:03:34 +0200

    Review the promote process with authentication

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -3,9 +3,10 @@ import handleAuth from "./auth";
 import { ClientOptions } from "./types";
 
 async function overrideConfig(): Promise<ClientOptions> {
-  if (process.env.VERCEL && process.env.VERCEL_ENV === "production") {
-    // Deployment to vercel production
-    if (process.env.VERCEL_DEPLOYMENT_ID && (await has(process.env.VERCEL_DEPLOYMENT_ID))) {
+  if (process.env.VERCEL && process.env.VERCEL_URL && process.env.VERCEL_ENV === "production") {
+    // When deploying to vercel production
+    // Check if the project is flagged as tested and promoted to select the correct base url
+    if (await has(process.env.VERCEL_URL)) {
       return {
         siteUrl: `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`,
         postLoginRedirectUrl: `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`,
