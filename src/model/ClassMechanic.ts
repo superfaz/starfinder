7af1c6d5a0876dd18d717f9494c2f9ec5deeb8a8
superfaz <16510828+superfaz@users.noreply.github.com>
2024-07-12 23:13:02 +0200

    Improve mechanic class

--- src/model/ClassMechanic.ts
+++ src/model/ClassMechanic.ts
@@ -1,10 +1,19 @@
 import { z } from "zod";
 import { FeatureTemplateSchema } from "./FeatureTemplate";
 import { IModelSchema } from "./IModel";
+import { DescriptionSchema, ReferenceSchema } from "./helper";
+import { INamedModelSchema } from "./INamedModel";
+
+export const ClassMechanicStyleSchema = INamedModelSchema.extend({
+  reference: ReferenceSchema,
+  description: DescriptionSchema,
+  features: z.optional(z.array(FeatureTemplateSchema)),
+}).strict();
 
 export const ClassMechanicSchema = IModelSchema.extend({
   id: z.string(),
   features: z.array(FeatureTemplateSchema),
+  styles: z.array(ClassMechanicStyleSchema),
 });
 
 export type ClassMechanic = z.infer<typeof ClassMechanicSchema>;
