ae4f4bb54b988b5f8d06de2b3c2f568c58935529
superfaz <16510828+superfaz@users.noreply.github.com>
2024-08-27 21:42:42 +0000

    Code review

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,4 @@
 import Link from "next/link";
-import Card from "react-bootstrap/Card";
-import CardBody from "react-bootstrap/CardBody";
-import CardLink from "react-bootstrap/CardLink";
-import CardText from "react-bootstrap/CardText";
-import CardTitle from "react-bootstrap/CardTitle";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
@@ -12,6 +7,7 @@ import NavbarBrand from "react-bootstrap/NavbarBrand";
 import NavLink from "react-bootstrap/NavLink";
 import Row from "react-bootstrap/Row";
 import AuthNavLink from "./AuthNavLink";
+import { Card } from "./components";
 
 export default function Page() {
   return (
@@ -45,55 +41,55 @@ export default function Page() {
             <Row className="my-5 g-2 row-cols-1 row-cols-md-2 row-cols-lg-3">
               <Col>
                 <Card className="h-100 text-bg-primary">
-                  <CardBody className="d-flex align-items-start flex-column">
-                    <CardTitle>Création libre</CardTitle>
-                    <CardText>Créez votre personnage avec un maximum de choix et de possibilités.</CardText>
+                  <Card.Body className="d-flex align-items-start flex-column">
+                    <Card.Title>Création libre</Card.Title>
+                    <Card.Text>Créez votre personnage avec un maximum de choix et de possibilités.</Card.Text>
                     <Link
                       href="/edit"
                       className="mt-auto btn btn-outline-dark stretched-link text-light icon-link icon-link-hover"
                     >
                       Démarrer la création <i className="bi bi-chevron-right mb-1"></i>
                     </Link>
-                  </CardBody>
+                  </Card.Body>
                 </Card>
               </Col>
               <Col>
                 <Card className="h-100">
-                  <CardBody className="d-flex align-items-start flex-column">
-                    <CardTitle>Basée sur un modèle</CardTitle>
-                    <CardText>
+                  <Card.Body className="d-flex align-items-start flex-column">
+                    <Card.Title>Basée sur un modèle</Card.Title>
+                    <Card.Text>
                       Choisissez un modèle de personnage et personnalisez celui-ci en fonction de vos envies.
-                    </CardText>
-                    <CardText>
+                    </Card.Text>
+                    <Card.Text>
                       <em>(Non implémenté)</em>
-                    </CardText>
-                    <CardLink
+                    </Card.Text>
+                    <Link
                       href="/edit"
                       className="disabled mt-auto btn btn-outline-primary stretched-link text-light icon-link icon-link-hover"
                     >
                       Démarrer la custo <i className="bi bi-chevron-right mb-1"></i>
-                    </CardLink>
-                  </CardBody>
+                    </Link>
+                  </Card.Body>
                 </Card>
               </Col>
               <Col>
                 <Card className="h-100">
-                  <CardBody className="d-flex align-items-start flex-column">
-                    <CardTitle>Pré-tirée</CardTitle>
-                    <CardText>
+                  <Card.Body className="d-flex align-items-start flex-column">
+                    <Card.Title>Pré-tirée</Card.Title>
+                    <Card.Text>
                       Utilisez l&apos;un des personnages pré-tirées pour démarrer directement votre personnage. Vous ne
                       pourrez pas le modifier sauf lors des montés de niveaux.
-                    </CardText>
-                    <CardText>
+                    </Card.Text>
+                    <Card.Text>
                       <em>(Non implémenté)</em>
-                    </CardText>
-                    <CardLink
+                    </Card.Text>
+                    <Link
                       href="/edit"
                       className="disabled mt-auto btn btn-outline-primary stretched-link text-light icon-link icon-link-hover"
                     >
                       Sélection <i className="bi bi-chevron-right mb-1"></i>
-                    </CardLink>
-                  </CardBody>
+                    </Link>
+                  </Card.Body>
                 </Card>
               </Col>
             </Row>
