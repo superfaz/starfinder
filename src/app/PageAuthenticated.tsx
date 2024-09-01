a91811a531c30e3cdf7c2402f4a4661de53f2f42
superfaz <16510828+superfaz@users.noreply.github.com>
2024-09-01 05:36:03 +0000

    Add navigation to characters list

--- src/app/PageAuthenticated.tsx
+++ src/app/PageAuthenticated.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
@@ -5,6 +6,7 @@ import { Licenses } from "./Licenses";
 import { CreateCards } from "./CreateCards";
 import { CharacterVM } from "./edit/viewmodel";
 import { CharacterCard } from "./edit/PageContent";
+import { Stack } from "react-bootstrap";
 
 export function PageAuthenticated({ characters }: Readonly<{ characters: CharacterVM[] }>) {
   return (
@@ -13,7 +15,12 @@ export function PageAuthenticated({ characters }: Readonly<{ characters: Charact
         <Col lg={8} className="mt-5">
           {characters.length > 0 && (
             <>
-              <h3>Personnages récents</h3>
+              <Stack direction="horizontal" gap={3}>
+                <h3>Personnages récents</h3>
+                <Link className="btn btn-primary" href="/edit">
+                  Voir tous
+                </Link>
+              </Stack>
               <Row className="row-cols-1 row-cols-sm-2 row-cols-md-3 mb-5">
                 {characters.map((character) => (
                   <Col key={character.id} className="mt-3">
