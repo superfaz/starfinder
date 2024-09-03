14a1a3783ba010c94f1d3a0864cf71941544cb3c
superfaz <16510828+superfaz@users.noreply.github.com>
2024-09-03 04:11:25 +0000

    Code review

--- src/app/edit/[character]/page.tsx
+++ src/app/edit/[character]/page.tsx
@@ -1,10 +1,10 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { type Metadata } from "next";
-import { isSecure } from "./helpers-server";
-import { PageContent } from "./PageContent";
+import { notFound } from "next/navigation";
 import { DataSets, DataSource, type IDataSource } from "data";
 import { IdSchema } from "model";
-import { notFound } from "next/navigation";
+import { isSecure } from "./helpers-server";
+import { PageContent } from "./PageContent";
 
 export const metadata: Metadata = {
   title: "Création",
