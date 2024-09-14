20fe9f092d188648154a1a3b3f80cb3dc209123d
superfaz <16510828+superfaz@users.noreply.github.com>
2024-09-14 21:48:37 +0200

    Code review

--- src/app/(main)/edit/actions.ts
+++ src/app/(main)/edit/actions.ts
@@ -1,17 +1,17 @@
 "use server";
 
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { DataSets, DataSource, IDataSource } from "data";
 import { revalidatePath } from "next/cache";
+import { DataSets, DataSource, IDataSource } from "data";
 
 export async function deleteCharacter(id: string) {
-    const { getUser } = getKindeServerSession();
-    const user = await getUser();
-    if (user === null) {
-        throw new Error("User not found");
-    }
-    
-    const dataSource: IDataSource = new DataSource();
-    dataSource.get(DataSets.Characters).delete({ id, userId: user.id });
-    revalidatePath("/edit");
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  if (user === null) {
+    throw new Error("User not found");
+  }
+
+  const dataSource: IDataSource = new DataSource();
+  dataSource.get(DataSets.Characters).delete({ id, userId: user.id });
+  revalidatePath("/edit");
 }
