5917228da3aafc8f8046d6bdbb3853f9e1ba469d
superfaz <16510828+superfaz@users.noreply.github.com>
2024-10-12 22:17:33 +0200

    Code review

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,26 +1,26 @@
+import { start, succeed } from "chain-of-actions";
 import { DataSets } from "data";
+import { getAuthenticatedUser, getDataSource } from "logic/server";
 import { ViewBuilder } from "view/server";
 import { PageContent } from "./PageContent";
 import { PageAuthenticated } from "./PageAuthenticated";
-import { start, succeed } from "chain-of-actions";
-import { getAuthenticatedUser, getDataSource } from "logic/server";
 
 export default async function Page() {
   const user = await getAuthenticatedUser();
 
   if (!user.success) {
     return <PageContent />;
-  } else {
-    const characters = await start(user.data)
-      .addData(() => getDataSource())
-      .addData(({ dataSource }) => succeed(new ViewBuilder(dataSource)))
-      .onSuccess(({ dataSource, user }) => dataSource.get(DataSets.Characters).find({ userId: user.id }, "updateOn", 3))
-      .runAsync();
-
-    if (!characters.success) {
-      throw new Error("Failed to load characters", characters.error);
-    } else {
-      return <PageAuthenticated characters={characters.data} />;
-    }
   }
+
+  const characters = await start(user.data)
+    .addData(() => getDataSource())
+    .addData(({ dataSource }) => succeed(new ViewBuilder(dataSource)))
+    .onSuccess(({ dataSource, user }) => dataSource.get(DataSets.Characters).find({ userId: user.id }, "updateOn", 3))
+    .runAsync();
+
+  if (!characters.success) {
+    throw new Error("Failed to load characters", characters.error);
+  }
+
+  return <PageAuthenticated characters={characters.data} />;
 }
