0d71a4829808f827cbc9a8ed129741d69498c0b5
superfaz <16510828+superfaz@users.noreply.github.com>
2024-10-13 07:45:35 +0200

    Integrate chain-of-actions

--- src/app/(main)/create/page.tsx
+++ src/app/(main)/create/page.tsx
@@ -1,24 +1,53 @@
 import { Metadata } from "next";
-import { isSecure } from "app/helpers-server";
+import { Result, start, succeed } from "chain-of-actions";
+import { DataSets } from "data";
 import { PageContent } from "./PageContent";
-import { DataSets, DataSource, IDataSource } from "data";
-import { ViewBuilder } from "view/server";
+import { getAuthenticatedUser, getDataSource, getViewBuilder, redirectToSignIn } from "logic/server";
+import { KindeUser } from "@kinde-oss/kinde-auth-nextjs/dist/types";
 
 export const metadata: Metadata = {
   title: "Création",
 };
 
+function getData<T, E extends Error>(result: Result<T[], E>) {
+  return result.success ? result.data : [];
+}
+
 export default async function Page() {
-  const returnTo = `/create`;
+  const context = await start()
+    .onSuccess(getAuthenticatedUser)
+    .onError<
+      {
+        user: KindeUser<Record<string, unknown>>;
+      },
+      never
+    >(() => redirectToSignIn(`/create`))
+    .addData(getDataSource)
+    .addData(({ dataSource }) => getViewBuilder(dataSource))
+    .runAsync();
 
-  if (await isSecure(returnTo)) {
-    const dataSource: IDataSource = new DataSource();
-    const builder = new ViewBuilder();
-
-    const races = builder.createRaceEntry(await dataSource.get(DataSets.Races).getAll());
-    const themes = builder.createEntry(await dataSource.get(DataSets.Themes).getAll());
-    const classes = builder.createEntry(await dataSource.get(DataSets.Class).getAll());
-
-    return <PageContent races={races} themes={themes} classes={classes} />;
+  if (!context.success) {
+    throw new Error("Failed to load context", context.error);
   }
+
+  const data = await Promise.all([
+    start(undefined, context.data)
+      .onSuccess((_, { dataSource }) => dataSource.get(DataSets.Races).getAll())
+      .onSuccess((races, { builder }) => succeed(builder.createRaceEntry(races)))
+      .runAsync(),
+    start(undefined, context.data)
+      .onSuccess((_, { dataSource }) => dataSource.get(DataSets.Themes).getAll())
+      .onSuccess((themes, { builder }) => succeed(builder.createEntry(themes)))
+      .runAsync(),
+    start(undefined, context.data)
+      .onSuccess((_, { dataSource }) => dataSource.get(DataSets.Class).getAll())
+      .onSuccess((classes, { builder }) => succeed(builder.createEntry(classes)))
+      .runAsync(),
+  ]);
+
+  if (data.some((d) => !d.success)) {
+    throw new Error("Failed to load data");
+  }
+
+  return <PageContent races={getData(data[0])} themes={getData(data[1])} classes={getData(data[2])} />;
 }
--- src/logic/server/services.ts
+++ src/logic/server/services.ts
@@ -1,8 +1,37 @@
 import { KindeUser } from "@kinde-oss/kinde-auth-nextjs/dist/types";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { convert, fail, PromisedResult, start, succeed } from "chain-of-actions";
+import { redirect } from "next/navigation";
+import { ZodError, ZodType, ZodTypeDef } from "zod";
 import { DataSource, IDataSource } from "data";
-import { UnauthorizedError } from "logic/errors";
+import { ParsingError, UnauthorizedError } from "logic/errors";
+import { ViewBuilder } from "view/server";
+
+export function parse<T, D extends ZodTypeDef, I>(
+  schema: ZodType<T, D, I>,
+  data: unknown
+): PromisedResult<T, ParsingError> {
+  return convert({
+    try: async () => schema.parse(data),
+    catch: (error) => {
+      if (error instanceof ZodError) {
+        return new ParsingError(error.flatten().fieldErrors);
+      } else {
+        throw error;
+      }
+    },
+  });
+}
+
+export function hasValidInput<T, D extends ZodTypeDef, I>(
+  schema: ZodType<T, D, I>,
+  data: unknown
+): PromisedResult<{ data: T }, ParsingError> {
+  return start()
+    .onSuccess(() => parse(schema, data))
+    .onSuccess(async (data) => succeed({ data }))
+    .runAsync();
+}
 
 export function getAuthenticatedUser(): PromisedResult<
   { user: KindeUser<Record<string, unknown>> },
@@ -21,4 +50,12 @@ export function getAuthenticatedUser(): PromisedResult<
 
 export function getDataSource(): PromisedResult<{ dataSource: IDataSource }, never> {
   return succeed({ dataSource: new DataSource() });
+}
+
+export function redirectToSignIn(returnTo: string): never {
+  redirect(`/api/auth/login?post_login_redirect_url=${encodeURIComponent(returnTo)}`);
+}
+
+export function getViewBuilder(dataSource: IDataSource): PromisedResult<{ builder: ViewBuilder }> {
+  return succeed({ builder: new ViewBuilder(dataSource) });
 }
