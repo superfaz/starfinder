b7738113540dfbcb0510b65410849e1b6adfe49f
superfaz <16510828+superfaz@users.noreply.github.com>
2024-10-16 21:16:03 +0200

    Code review

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import * as Sentry from "@sentry/nextjs";
+import { UnauthorizedError } from "logic";
 import Error from "next/error";
 import { useEffect } from "react";
 
@@ -8,6 +9,16 @@ export default function GlobalError({ error }: Readonly<{ error: Error }>) {
   useEffect(() => {
     Sentry.captureException(error);
   }, [error]);
+
+  if (Error instanceof UnauthorizedError) {
+    return (
+      <html lang="fr">
+        <body>
+          <Error statusCode={401} />
+        </body>
+      </html>
+    );
+  }
 
   return (
     <html lang="fr">
